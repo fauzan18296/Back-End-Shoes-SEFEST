2eba26dc50411cbe8b357c3274ae4914243ce079
Ahmad Fauzan <fauzan18296@gmail.com>
2024-07-03 16:26:27 +0700

    add: merubah code di back-end

--- src/main.js
+++ src/main.js
@@ -5,22 +5,29 @@ import bodyParser from 'body-parser'
 const app = express()
 app.use(cors())
 app.use(bodyParser.json({extended:true}))
-
-app.post('/comment' , (req , res) => {
-  const data = {
-    username: req.body.username,
-    nickname: req.body.nickname,
-    comment: req.body.comment,
-  }
-  db.query(`INSERT INTO commentar(username,nickname,comment) VALUES('${data.username}','${data.nickname}','${data.comment}')`, (results) => {
-      if(results) {
-        res.send({results:data})
-      }else {
-      res.send({message: results})
+app.use('/api/', (req, res, next) => {
+  if (!req.is('application/json')) {
+      // Send error here
+      res.send(400);
+  } else {
+     app.post('/comment' , (req , res) => {
+      const data = {
+        username: req.body.username,
+        nickname: req.body.nickname,
+        comment: req.body.comment,
       }
-    }
-  )
-})
+      db.query(`INSERT INTO commentar(username,nickname,comment) VALUES('${data.username}','${data.nickname}','${data.comment}')`, (results) => {
+          if(results) {
+            res.send({results:data})
+          }else {
+          res.send({message: results})
+          }
+        }
+      )
+    })
+  }
+});
+
 
 app.listen(3000, () => {
   console.log(`Server succes connected in port 3000!`)
