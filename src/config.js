f754d6c1501f086a6d7305ea90e086d183f1c951
Ahmad Fauzan <fauzan18296@gmail.com>
2024-07-03 16:41:29 +0700

    add: merubah code di back-end

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql';
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "id22401126_db_elegant_shoe_shop",
-  password: "aTBC3N+#aBX@.Yi",
-  database: "id22401126_db_elegant_shoe_shop_api_1",
+  user: "root",
+  password: "",
+  database: "db_back-end-shoes-lomba-sefest",
 })
 
 connection.connect(() => {
